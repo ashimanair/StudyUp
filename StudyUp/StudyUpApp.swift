6ec8059b30083c8f4b8856d097b6749dec04e1ef
Emily Zheng <eezz7519@gmail.com>
2024-06-11 09:51:08 -0500

    Initial Commit

---
+++ StudyUp/StudyUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyUpApp.swift
+//  StudyUp
+//
+//  Created by Scholar on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
